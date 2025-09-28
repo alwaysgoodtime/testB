a7654c682e2a8ce4a5f929252ccc4fd42d0aba2b
郝建宇 <haojianyu@secidea.com>
2025-09-28 18:39:19 +0800

    增加b库

---
+++ b.c
@@ -0,0 +1,6 @@
+#include "b.h"
+
+// B库的实际实现
+int b_calculate_sum(int a, int b) {
+    return a + b;
+}
---
+++ b.h
@@ -0,0 +1,7 @@
+#ifndef B_H
+#define B_H
+
+// B库的函数声明
+int b_calculate_sum(int a, int b);
+
+#endif // B_H
